8f73898d265c6492d96668c766d0e2e2c15ca264
EEБ <none@none>
2019-12-27 17:48:02 +0300

    almost done risWatcher

--- RISWatcher/lout.cpp
+++ RISWatcher/lout.cpp
@@ -1,8 +1,27 @@
 #include "lout.h"
 
 #include <iomanip>
+
+static constexpr size_t nExtraChars=25+8+6;
+
+#ifdef _WIN32
+#include <windows.h>
+size_t getWidth()
+{
+    CONSOLE_SCREEN_BUFFER_INFO nfo;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&nfo);
+    return nfo.srWindow.Right-nfo.srWindow.Left-nExtraChars;
+}
+#else
 #include <sys/ioctl.h> //ioctl() and TIOCGWINSZ
 #include <unistd.h> // for STDOUT_FILENO
+size_t getWidth()
+{
+    struct winsize size;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+    return size.ws_col-nExtraChars;
+}
+#endif
 
 using namespace std;
 
@@ -11,13 +30,6 @@ Lout lout;
 std::ostream &operator <<(std::ostream &out, const QString &str)
 {
     return out << str.toUtf8().toStdString();
-}
-
-size_t getWidth()
-{
-    struct winsize size;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-    return size.ws_col-25-8-6;
 }
 
 std::ostream &operator <<(std::ostream &out, const std::string &in)
@@ -29,11 +41,20 @@ std::ostream &operator <<(std::ostream &out, const std::string &in)
         i+=width;
         if(i>=in.size())
         {
-            out << std::left << std::setfill(' ') << std::setw(width);
+            out << std::left << std::setfill(' ') << std::setw(width+Lout::brWidth+2);
             std::operator<<(out, in.substr(j));
+            out << flush;
             return out;
         }
         out << in.substr(j,i) << '\n' << std::left << std::setfill(' ') << std::setw(29);
+    }
+}
+
+void Lout::nextTick()
+{
+    if(++curTick==tickChars.cend())
+    {
+        curTick=tickChars.cbegin();
     }
 }
 
@@ -49,16 +70,18 @@ void Lout::anounse(const QString &msg)
 
 void Lout::brackets(const string &str)
 {
+
     const auto midpos=str.length()/2;
     const auto strHalfL=str.substr(0,midpos);
     const auto strHalfR=str.substr(midpos);
 
     constexpr size_t half=brWidth/2;
-    cout<<'['<<setw(half)<<right;
+
+    cout << setfill('\b') << setw(brWidth+1) << '\b' << setfill(' ') << '['<<setw(half)<<right;
     std::operator<<(cout,strHalfL);
     cout<<setw(half)<<left;
     std::operator<<(cout,strHalfR);
-    cout<<']';
+    cout<<']'<<flush;
 
 }
 
@@ -74,5 +97,15 @@ void Lout::fail()
 
 void Lout::tick()
 {
-    cout << setfill('\b') << setw(brWidth+1) << '\b';
+    brackets(std::string(curTick,1));
+    nextTick();
+}
+
+void Lout::percent(const size_t cur, const size_t total)
+{
+    const size_t percent = 100 * cur / total;
+    stringstream str;
+    str<< *curTick<<' '<<setw(3)<<percent<<'%';
+    brackets(str.str());
+    nextTick();
 }
--- RISWatcher/lout.h
+++ RISWatcher/lout.h
@@ -7,25 +7,7 @@
 #include <iomanip>
 #include <array>
 #include <QString>
-
-template<typename Clock, typename Duration>
-std::ostream &operator<<(std::ostream &stream,
-  const std::chrono::time_point<Clock, Duration> &time_point)
-{
-  const time_t time = Clock::to_time_t(time_point);
-#if __GNUC__ > 4 || \
-    ((__GNUC__ == 4) && __GNUC_MINOR__ > 8 && __GNUC_REVISION__ > 1)
-  // Maybe the put_time will be implemented later?
-  struct tm tm;
-  localtime_r(&time, &tm);
-  return stream << std::put_time(&tm, "%c"); // Print standard date&time
-#else
-  char buffer[26];
-  ctime_r(&time, buffer);
-  buffer[24] = '\0';  // Removes the newline that is added
-  return stream << buffer;
-#endif
-}
+#include <QDateTime>
 
 std::ostream& operator << (std::ostream& out,const std::string& in);
 
@@ -38,19 +20,21 @@ class Lout
         return std::chrono::system_clock::now();
     }
     const std::array<char,4> tickChars = {'|','/','-','\\'};
-    static constexpr size_t brWidth=6;
     std::array<char,4>::const_iterator curTick=tickChars.cbegin();
-public:    
+    void nextTick();
+public:
+    static constexpr size_t brWidth=6;
     template<typename T> std::ostream& operator << (const T& rhs)
     {
-        return std::cout << '['<< tm() << "]     " << rhs;
+        return std::cout << '['<< QDateTime::currentDateTime().toString() << "]     " << rhs;
     }
     void anounse(const std::string& msg);
     void anounse(const QString& msg);
-    void brackets(const std::string& str);
+    void brackets(const std::string& str=std::string());
     void ok();
     void fail();
     void tick();
+    void percent(const size_t cur,const size_t total);
 };
 
 extern Lout lout;
