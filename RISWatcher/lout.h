d4d84581f3bec243942de66729aff99760b001c9
EEБ <none@none>
2020-04-26 09:55:07 +0300

    increase debug lvl

--- RISWatcher/lout.h
+++ RISWatcher/lout.h
@@ -32,7 +32,7 @@ private:
     void nextTick();        
     size_t lastX=0;
     LogLevel msgLevel=Info;
-    LogLevel outLevel=Info;
+    LogLevel outLevel=Trace;
 public:   
     void shift(size_t count);
     void resetX();
